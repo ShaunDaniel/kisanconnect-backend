af4f102f058ef8c292c225dcde0ef7d3fdee762a
ShaunDaniel <shaun.daniel@science.christuniversity.in>
2024-09-08 19:46:09 +0530

    helmet

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes.js'; // Add .js extension
 import mongooseConnection from './config/mongoDB.js'; // Add .js extension
+import helmet from 'helmet';
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,8 +15,21 @@ app.use(morgan('dev'));
 
 dotenv.config();
 
-const allowedOrigins = ['http://localhost:5173', 'https://kisanconnect-pi.vercel.app/'];
-
+const allowedOrigins = ['http://localhost:5173', 'https://kisanconnect-pi.vercel.app'];
+app.use(helmet()); // Apply helmet middleware for security headers
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "validator.swagger.io"],
+        childSrc: ["'none'"],
+        formAction: ["'self'"],
+        frameAncestors: ["'none'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+    }
+}));
 // Middleware to set headers
 app.use(cors({
     origin:allowedOrigins,
