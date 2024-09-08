c456526f9177a664fd490dfa639ae1448a442664
ShaunDaniel <shaun.daniel@science.christuniversity.in>
2024-09-08 19:28:41 +0530

    issues with cors

--- src/index.js
+++ src/index.js
@@ -5,49 +5,25 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes.js'; // Add .js extension
 import mongooseConnection from './config/mongoDB.js'; // Add .js extension
-import helmet from 'helmet'; 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(morgan('dev'));
 
 dotenv.config();
-const allowedOrigins = [
-    'http://localhost:5173',
-    `${process.env.FRONTEND_URL}`,
-    'https://kisanconnect-pi.vercel.app'    // Add this line
-];
 
-// Middleware
-app.set('trust proxy', 1);
-app.use(helmet()); // Apply helmet middleware for security headers
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", "data:", "validator.swagger.io"],
-        childSrc: ["'none'"],
-        formAction: ["'self'"],
-        frameAncestors: ["'none'"],
-        objectSrc: ["'none'"],
-        upgradeInsecureRequests: [],
-    }
-}));
+const allowedOrigins = ['http://localhost:5173', 'https://kisanconnect-pi.vercel.app/'];
+
+// Middleware to set headers
 app.use(cors({
-    origin: function(origin, callback){
-        console.log(origin);
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        res.header('Access-Control-Allow-Origin', origin); // Set the allowed origin
-        return callback(null, true);
-    },
-    credentials: true
+    origin:allowedOrigins,
+    methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
+    allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }));
+
+app.set('trust proxy', 1);
 
 
 app.use(bodyParser.json());
